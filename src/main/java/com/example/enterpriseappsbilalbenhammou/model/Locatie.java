2b587b2e4e978e7a92c88d8a1f695671551cbbd1
benbilal94 <benhambilal94@gmail.com>
2026-06-09 21:53:54 +0200

    entiteiten aangemaakt

---
+++ src/main/java/com/example/enterpriseappsbilalbenhammou/model/Locatie.java
@@ -0,0 +1,56 @@
+package com.example.enterpriseappsbilalbenhammou.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "locaties")
+public class Locatie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private int capacity;
+
+    public Locatie(String name, String address, int capacity) {
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
