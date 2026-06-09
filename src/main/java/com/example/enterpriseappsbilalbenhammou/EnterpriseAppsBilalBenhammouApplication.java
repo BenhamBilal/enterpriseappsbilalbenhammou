ff58b970ba35c665b1f467c3d6b49d2a53341046
benbilal94 <benhambilal94@gmail.com>
2026-06-09 21:31:45 +0200

    first commit

---
+++ src/main/java/com/example/enterpriseappsbilalbenhammou/EnterpriseAppsBilalBenhammouApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseappsbilalbenhammou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppsBilalBenhammouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseAppsBilalBenhammouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enterpriseappsbilalbenhammou/EnterpriseAppsBilalBenhammouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseappsbilalbenhammou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAppsBilalBenhammouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
